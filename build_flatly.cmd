1aca8a752a38b528d4edf72f1da800d603894b4c
nerk <kern99@web.de>
2014-01-13 00:33:12 +0100

    Fixes

--- build_flatly.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem call lessc src\less\styles\slate\styles.less css\bootstrap_slate.css
-call lessc src\less\styles\flatly\styles.less css\bootstrap_flatly.css
-call lessc src\less\styles\default_themed\styles.less css\bootstrap_default_themed.css
-pause
