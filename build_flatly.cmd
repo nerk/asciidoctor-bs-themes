a734c69731168889283f651a2b98e33388de9190
nerk <kern99@web.de>
2014-01-13 00:17:48 +0100

    Bootswatch themes use bootswatch fork of bootstrap

---
+++ build_flatly.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+rem call lessc src\less\styles\slate\styles.less css\bootstrap_slate.css
+call lessc src\less\styles\flatly\styles.less css\bootstrap_flatly.css
+call lessc src\less\styles\default_themed\styles.less css\bootstrap_default_themed.css
+pause
--- test/gendoc.cmd
+++ test/gendoc.cmd
@@ -5,9 +5,7 @@ set styles=default default_themed amelia cerulean cosmo cyborg flatly journal re
 IF NOT EXIST output MD output
 
 FOR %%A IN (%styles%) DO (
-    rem asciidoctor -T ..\src\templates -a source-highlighter="coderay" -a linkcss  -a icons="font" -a stylesheet="../../css/bootstrap_%%A.css" -o output\userguide_%%A.html userguide.adoc
 	rem asciidoctor -T ..\src\templates -a source-highlighter="coderay" -a linkcss  -a icons="font" -a stylesheet="../../css/bootstrap_%%A.css" -o output\test_%%A.html test.adoc
 	
-	asciidoctor -a stylesheet="../css/bootstrap_%%A.css" -o output\userguide_%%A.html userguide.adoc
 	asciidoctor -a stylesheet="../css/bootstrap_%%A.css" -o output\test_%%A.html test.adoc
 )
