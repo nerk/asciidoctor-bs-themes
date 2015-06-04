ca1d25056984950277b738de15c80bfc62411d88
nerk <kern99@web.de>
2015-06-04 23:06:45 +0200

    updated bootswatch styles and template

--- buildcss.cmd
+++ buildcss.cmd
@@ -1,9 +1,10 @@
 @echo off
 
-set styles=default default_themed amelia cerulean cosmo cyborg darkly flatly journal lumen united readable simplex slate spacelab superhero yeti
+set styles=default default_themed cerulean cosmo custom cyborg darkly flatly journal lumen paper readable sandstone simplex slate spacelab superhero united yeti
 
 FOR %%A IN (%styles%) DO (
    echo creating css\bootstrap_%%A.css
-   call lessc --global-var="bsw-root='https://raw.githubusercontent.com/thomaspark/bootswatch/gh-pages'" src\less\styles\%%A\styles.less css\bootstrap_%%A.css
+   REM current lessc produces some warnings due to a bug. -s silences this
+   call lessc -s --global-var="bsw-root='https://raw.githubusercontent.com/thomaspark/bootswatch/gh-pages'" src\less\styles\%%A\styles.less css\bootstrap_%%A.css
    call cleancss -o css\bootstrap_%%A.min.css css\bootstrap_%%A.css
 )
--- test/gendoc.cmd
+++ test/gendoc.cmd
@@ -1,8 +1,9 @@
 @echo off
 
-set styles=default default_themed amelia cerulean cosmo cyborg darkly flatly journal lumen united readable simplex slate spacelab superhero yeti
+set styles=default default_themed cerulean cosmo custom cyborg darkly flatly journal lumen paper readable sandstone simplex slate spacelab superhero united yeti
 
 IF NOT EXIST output MD output
+DEL /Q output\*
 
 FOR %%A IN (%styles%) DO (
 	rem asciidoctor -T ..\src\templates -a source-highlighter="coderay" -a linkcss  -a icons="font" -a stylesheet="../../css/bootstrap_%%A.css" -o output\test_%%A.html test.adoc
