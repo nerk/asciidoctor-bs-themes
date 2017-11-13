efd32376745fd1f4015e3c18f9c9b5d85325009f
nerk <kern99@web.de>
2017-11-13 23:22:27 +0100

    Fixes #4, #5
    Fix path to bootswatch repository.

--- buildcss.cmd
+++ buildcss.cmd
@@ -5,6 +5,6 @@ set styles=default default_themed cerulean cosmo custom cyborg darkly flatly jou
 FOR %%A IN (%styles%) DO (
    echo creating css\bootstrap_%%A.css
    REM current lessc produces some warnings due to a bug. -s silences this
-   call lessc -s --global-var="bsw-root='https://raw.githubusercontent.com/thomaspark/bootswatch/gh-pages'" src\less\styles\%%A\styles.less css\bootstrap_%%A.css
+   call lessc -s --global-var="bsw-root='https://raw.githubusercontent.com/thomaspark/bootswatch/v3.3.7'" src\less\styles\%%A\styles.less css\bootstrap_%%A.css
    call cleancss -o css\bootstrap_%%A.min.css css\bootstrap_%%A.css
 )
